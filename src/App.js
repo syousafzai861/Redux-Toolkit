5e420a970b3555961864160755d26123c5e69e0a
Saif Ul islam <91761499+syousafzai861@users.noreply.github.com>
2023-02-02 15:34:10 +0500

    increment / decrement Done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Counter from "./features/counter/Counter";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>HELLO REDUX TOOLKIT</h1>
+     <Counter/>
+    </div>
+  );
+}
+
+export default App;
